42d8b323c920cc9286d29586dde2ebe24b06d482
Jonathan Evoy <contactjonathanevoy@gmail.com>
2026-02-26 14:45:28 -0500

    Chef.js: createHeader, createListItem, createFooter
    Tout cela a été fait, youpi!
    En théorie, il reste juste Poutine.js puis je peux revérifier mon code itout.

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -5,10 +5,12 @@ export default class Chef {
     this.element = element;
     this.menu = [];
     this.container = this.element.querySelector('.js-chef__order');
+    this.poutinesActive = 0;
     this.init();
   }
   init() {
     const poutines = this.element.querySelectorAll('.js-poutine');
+
     for (let i = 0; i < poutines.length; i++) {
       const poutine = new Poutine(poutines[i]);
       this.menu.push(poutine);
@@ -19,16 +21,32 @@ export default class Chef {
   }
   sendOrder() {
     this.container.innerHTML = '';
+    this.poutinesActive = 0;
 
-    var poutinesActive = 0;
+    this.createHeader();
+
     for (let i = 0; i < this.menu.length; i++) {
       if (this.menu[i].selectedType) {
-        poutinesActive++;
+        this.poutinesActive++;
+        this.createListItem(i);
       }
     }
 
+    this.createFooter();
+  }
+  createHeader() {
+    const header = document.createElement('h2');
+    header.innerText = 'Voici le résumé de votre commande :';
+    this.container.appendChild(header);
+  }
+  createListItem(poutineChecked) {
     const p = document.createElement('p');
-    p.innerText = `Nombre total de poutine(s) :  ${poutinesActive}`;
+    p.innerText = `Poutine #${this.poutinesActive} - ${this.menu[poutineChecked].selectedType}`;
+    this.container.appendChild(p);
+  }
+  createFooter() {
+    const p = document.createElement('p');
+    p.innerText = `Nombre total de poutine(s) :  ${this.poutinesActive}`;
     this.container.appendChild(p);
   }
 }
