89ab40cf5faaebbd1bde00bd118849eb44f5a447
Jonathan Evoy <contactjonathanevoy@gmail.com>
2026-02-26 14:06:50 -0500

    Poutine.constructor() + changement de data attributes
    Le constructor de la classe Poutine a été créé.
    Les data-attributes ont été changés: maintenant on sélectionne les poutines avec .js-poutine et on trouve le type dans Poutine.selectedType. Les boutons ont des data-attributes pour changer le type plus tard.

--- src/scripts/Chef.js
+++ src/scripts/Chef.js
@@ -8,7 +8,7 @@ export default class Chef {
     this.init();
   }
   init() {
-    const poutines = this.element.querySelectorAll('[data-poutine]');
+    const poutines = this.element.querySelectorAll('.js-poutine');
     for (let i = 0; i < poutines.length; i++) {
       const poutine = new Poutine(poutines[i]);
       this.menu.push(poutine);
--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,5 +1,8 @@
 export default class Poutine {
   constructor(element) {
+    this.element = element;
+    this.types = this.element.querySelectorAll('[data-type]');
+    this.selectedType = '';
     this.init();
   }
   init() {}
