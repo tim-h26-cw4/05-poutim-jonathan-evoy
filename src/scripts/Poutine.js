132c53e82c848fbbb7ad70195c7d07cd75285177
Jonathan Evoy <contactjonathanevoy@gmail.com>
2026-02-26 14:21:14 -0500

    Poutine.js: init, selectType et updatePhoto construit
    Encore une fois, j'ai du niaiser avec les data attributes et les class. J'espère que c'est final maintenant, mais prob pas.

--- src/scripts/Poutine.js
+++ src/scripts/Poutine.js
@@ -1,9 +1,29 @@
 export default class Poutine {
   constructor(element) {
     this.element = element;
-    this.types = this.element.querySelectorAll('[data-type]');
+    this.types = this.element.querySelectorAll('.js-type-button');
     this.selectedType = '';
     this.init();
   }
-  init() {}
+  init() {
+    for (let i = 0; i < this.types.length; i++) {
+      this.types[i].addEventListener('click', this.selectType.bind(this));
+    }
+  }
+  selectType(event) {
+    for (let i = 0; i < this.types.length; i++) {
+      this.types[i].classList.remove('is-active');
+    }
+
+    const target = event.currentTarget;
+    target.classList.add('is-active');
+
+    this.selectedType = target.innerText;
+    this.updatePhoto();
+  }
+  updatePhoto() {
+    const image = this.element.querySelector('.js-poutine__image');
+    image.classList.add('is-active');
+    image.src = `assets/images/${this.selectedType}.png`;
+  }
 }
